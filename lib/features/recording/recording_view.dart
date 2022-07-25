d73322045b7a918ff8850a72f2a18984e0f79402
paul.kim <paul.kim@honeynaps.com>
2022-07-25 11:18:43 +0900

    feat: Impl recording_view abstract structure

--- lib/features/recording/recording_view.dart
+++ lib/features/recording/recording_view.dart
@@ -22,36 +22,154 @@ class RecordingView extends StatelessWidget {
           ),
         ),
         onTap: () {
-          Navigator.of(context).pop();
+          if (Navigator.of(context).canPop()) {
+            Navigator.of(context).pop();
+          }
         },
       ),
     );
   }
 
+  Widget _recordingColumnView(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        // Camera
+        Expanded(
+          flex: 1,
+          child: _videoRecordingView(context),
+        ),
+        Expanded(
+          flex: 1,
+          child: _sentenceView(context),
+        ),
+      ],
+    );
+  }
+
+  Widget _recordingRowView(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        // Camera
+        Expanded(
+          flex: 1,
+          child: _videoRecordingView(context),
+        ),
+        Expanded(
+          flex: 1,
+          child: _sentenceView(context),
+        ),
+      ],
+    );
+  }
+
+  Widget _videoRecordingView(BuildContext context) {
+    return const Text("This is CameraView");
+  }
+
+  Widget _sentenceView(BuildContext context) {
+
+    const List<String> sentences = [
+      "안녕",
+      "너는보니?",
+      "반갑다",
+      "안녕",
+      "너는보니?",
+      "반갑다",
+      "안녕",
+      "너는보니?",
+      "반갑다",
+      "안녕",
+      "너는보니?",
+      "반갑다",
+      "안녕",
+      "너는보니?",
+      "반갑다",
+      "안녕",
+      "너는보니?",
+      "반갑다",
+      "안녕",
+      "너는보니?",
+      "반갑다",
+      "안녕",
+      "너는보니?",
+      "반갑다",
+      "안녕",
+      "너는보니?",
+      "반갑다",
+      "안녕",
+      "너는보니?",
+      "반갑다",
+      "안녕",
+      "너는보니?",
+      "반갑다",
+      "안녕",
+      "너는보니?",
+      "반갑다",
+      "안녕",
+      "너는보니?",
+      "반갑다",
+      "안녕",
+      "너는보니?",
+      "반갑다",
+      "안녕",
+      "너는보니?",
+      "반갑다",
+      "안녕",
+      "너는보니?",
+      "반갑다",
+    ];
+
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: sentences.asMap().entries.map((entry) {
+          int idx = entry.key;
+          String sentence = entry.value;
+          return Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              boxShadow: Effects.boxShadowEffect(context),
+            ),
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+            child: Text(sentence),
+          );
+        }).toList()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
-
     const double marginHorizontal = 80.0;
     const double marginVertical = 40.0;
 
+    MediaQueryData mqData = MediaQuery.of(context);
+    double height = mqData.size.height;
+    double width = mqData.size.width;
+    double whRatio = width / height;
+
+    Widget recordingView = (whRatio > 1.2)
+        ? _recordingRowView(context)
+        : _recordingColumnView(context);
+
     return Scaffold(
       body: Stack(children: [
+        /// Main view (Camera view, contetns view)
         Container(
-          color:
-              Theme.of(context).colorScheme.primaryContainer.withOpacity(0.2),
           margin: const EdgeInsets.symmetric(
             horizontal: marginHorizontal,
             vertical: marginVertical,
           ),
           width: double.infinity,
           height: double.infinity,
-          child: SizedBox(),
+          child: recordingView,
         ),
+
+        /// Backward Button
         _backBtn(context),
-
-
       ]),
     );
   }
